6ba92daf97b5ce3c9b018ce28ee9ba9a623a0ffb
jang donghyeok <commitnpush@gmail.com>
2018-06-22 11:29:55 +0900

    회원가입 기능 추가 1.member테이블 추가 2.MemberController, MemberService, MemverDao 3./member/signup(get) 4.가입화면 구현 5./member/signup(post) 6.vaildation(기본+중복확인+이메일인증+이메일중복) 7.business로직,persistence로직

--- dayary.sql
+++ dayary.sql
@@ -1,7 +1,9 @@
+
 create user commitnpush identified by 11111111;
 
 grant dba to commitnpush;
 
+--diary
 create table diary(
     id number primary key,
     u_id varchar2(15),
@@ -18,8 +20,19 @@ values(seq_diary_id.nextval, 'flynn', '첫 번째 일기',
 insert into diary 
 values(seq_diary_id.nextval, 'flynn', '두 번째 일기', 
        '곧 프로젝트를 시작해야 한다', sysdate);
+commit;
+delete diary;
 
-commit;       
-       
+select * from diary;
+
+--member
+create table member(
+    id varchar2(10) primary key,
+    password varchar2(10) not null,
+    name varchar2(30) not null,
+    email varchar2(30) not null unique,
+    gender char(1) check(gender in('m', 'f'))
+);
+    
 
 
